9d6951d5672ffcd03950d895aaf7272cf0f74870
san-sanchez <sanchez.santiago.dev@gmail.com>
2025-08-05 20:07:40 -0300

    agregados de css en index

---
+++ Js/locales.js
@@ -0,0 +1,9 @@
+const ciudad = document.getElementById("ciudad");
+
+const ubicacion = document.getElementById("ubi_local");
+
+const horario = document.getElementById("hr_local");
+
+const img = document.querySelectorAll("local_Img");
+
+img.
