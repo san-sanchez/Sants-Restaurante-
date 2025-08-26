f157116e512cc4bd42092542181b56d0c1441c84
san-sanchez <sanchez.santiago.dev@gmail.com>
2025-08-25 22:49:55 -0300

    no

--- Js/loopFotos.js
+++ Js/loopFotos.js
@@ -3,10 +3,47 @@ let images =["../img/historia/images.jfif","../img/historia/familia.jpg","../img
 
 let index=0;
 
+
 window.addEventListener("DOMContentLoaded",()=>{
     setInterval(()=>{
-    const $foto = document.getElementById("modificar-foto");
+    const $foto = document.getElementById("modificar-foto-A");
     index = (index+ 1)%images.length;
     $foto.src=images[index];
-    },10000);
+    },1000);
+})
+window.addEventListener("DOMContentLoaded",()=>{
+    setInterval(()=>{
+    const $foto = document.getElementById("modificar-foto-B");
+    index = (index+ 1)%images.length;
+    $foto.src=images[index];
+    },2500);
+})
+window.addEventListener("DOMContentLoaded",()=>{
+    setInterval(()=>{
+    const $foto = document.getElementById("modificar-foto-C");
+    index = (index+ 1)%images.length;
+    $foto.src=images[index];
+    },3000);
+})
+window.addEventListener("DOMContentLoaded",()=>{
+    setInterval(()=>{
+    const $foto = document.getElementById("modificar-foto-D");
+    index = (index+ 1)%images.length;
+    $foto.src=images[index];
+    },1000);
+})
+window.addEventListener("DOMContentLoaded",()=>{
+    setInterval(()=>{
+    const $foto = document.getElementById("modificar-foto-E");
+    index = (index+ 1)%images.length;
+    $foto.src=images[index];
+    },2000);
+})
+
+window.addEventListener("DOMContentLoaded",()=>{
+    setInterval(()=>{
+    const $foto = document.getElementById("modificar-foto-F");
+    index = (index+ 1)%images.length;
+    $foto.src=images[index];
+    },3000);
 })
